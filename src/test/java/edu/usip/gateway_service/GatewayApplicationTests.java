c15be5818d5269e1221b9333eb9bd751429de278
Alejandro Salazar <asalazarm@pilandina.com.bo>
2026-03-08 01:26:18 -0400

    first commit

---
+++ src/test/java/edu/usip/gateway_service/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usip.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
